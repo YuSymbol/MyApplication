1c33aae05a3b4ad33b77b432d796e2d27d066f44
Young <fuyuxyfy@163.com>
2019-09-21 01:28:24 +0800

    some comments and demo of LOG; delete WeatherAPI.java for a moment

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    //  输入logt，利用tab会自动补全当前的类名
+    private static final String TAG = "MainActivity";
     //  main activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  打印日志的功能，必须学会习惯使用
+        //  在Logcat栏显示，可以通过筛选重要级别和关键字（例如类名以分类）来查看
+        Log.d(TAG,"onCreate execute");
+
     }
 }
